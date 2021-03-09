aa46afbebff6a2a2e84ef09e011840d13d53ff2b
Robby <robby@santoslab.org>
2021-03-09 17:06:24 -0600

    Refactored smt2 and check non-generic strict pure methods first.

--- jvm/src/main/scala/org/sireum/server/service/LogikaService.scala
+++ jvm/src/main/scala/org/sireum/server/service/LogikaService.scala
@@ -311,7 +311,7 @@ object LogikaService {
     }
     val config = defaultConfig
     logika.Logika.checkFile(req.uriOpt, req.content, config, (th: lang.tipe.TypeHierarchy) =>
-      logika.Smt2Impl(defaultConfig.smt2Configs, th, scriptCache, config.timeoutInMs, config.charBitWidth,
+      logika.Smt2Impl.create(defaultConfig.smt2Configs, th, scriptCache, config.timeoutInMs, config.charBitWidth,
         config.intBitWidth, config.simplifiedQuery), reporter, T, hasLogika)
   }
 }
